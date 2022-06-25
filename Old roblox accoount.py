c7ce48088647bee3159a33cdb4cddb980a05ffce
Hacer00700 <107203216+Hacer00700@users.noreply.github.com>
2022-06-25 13:09:26 -0600

    Add files via upload
    This is an old roblox account with a 10 robux avatar if you need more use blox.land

---
+++ Old roblox accoount.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+"""Blank Python 3.x file"""
+print ("My password is yellow987")
+first_name = "minioreo5689"
+print (first_name)
+is_online = True
+print (is_online)
+print ("The email adress is fake. I got it from fakemail.net")
